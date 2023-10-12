c9d5c17b7331541b512250d077442658967e67d5
Gavin Kelly <gavin.kelly@crick.ac.uk>
2023-10-12 14:31:30 +0100

    Add debugging logs

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ async function run() {
     // The YML workflow will need to set myToken with the GitHub Secret Token
     // myToken: ${{ secrets.GITHUB_TOKEN }}
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    console.log("Getting token...");
     const myToken = core.getInput('myToken');
-
+    console.log("Getting Octokit...");
     const octokit = github.getOctokit(myToken);
 
     // You can also pass in additional options as a second parameter to getOctokit
     // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
-    
+  
+    console.log("Getting topics...");
+    console.log(JSON.stringify(context));
     const topics = await octokit.rest.repos.getAllTopics({...context});
     
     if (topics.names.indexOf("changed") === -1) {
+	    console.log("Settings topics...");
+
 	topics.names.push("changed");
 	await octokit.rest.repos.replaceAllTopics({...context, ...topics});
     }
