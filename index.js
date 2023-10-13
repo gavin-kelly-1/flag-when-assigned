34a571e627095f35fcae1733cc60567b859dfb1f
Gavin Kelly <gavin.kelly@crick.ac.uk>
2023-10-13 13:50:51 +0100

    Look into the github event

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const core = require('@actions/core');
-// const github = require('@actions/github');
+const github = require('@actions/github');
+console.log(JSON.stringify(github.event));
 // const context = github.context;
 
 // async function run() {
